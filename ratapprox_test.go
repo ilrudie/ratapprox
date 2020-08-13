537e3aee226609282a1d2301f1c253ce52dda4f0
Ian Rudie <ilrudie+git@gmail.com>
2020-08-13 15:43:53 -0400

    Additional testing added

--- ratapprox_test.go
+++ ratapprox_test.go
@@ -19,3 +19,33 @@ func TestFarey(t *testing.T) {
 		t.Errorf("farey() = %q, want %q", got, want)
 	}
 }
+
+func TestDivFraction(t *testing.T) {
+	want := .5
+	if got := (Fraction{1, 2}.divFraction()); got != want {
+		t.Errorf("divFraction() = %f, want %f", got, want)
+	}
+	want = .125
+	if got := (Fraction{1, 8}.divFraction()); got != want {
+		t.Errorf("divFraction() = %f, want %f", got, want)
+	}
+}
+
+func TestRatApprox(t *testing.T) {
+	want := Fraction{1, 2}
+	if got := RatApprox(.5); got != want {
+		t.Errorf("farey() = %q, want %q", got, want)
+	}
+	want = Fraction{1, 8}
+	if got := RatApprox(.125); got != want {
+		t.Errorf("farey() = %q, want %q", got, want)
+	}
+	want = Fraction{19, 20}
+	if got := RatApprox(.95); got != want {
+		t.Errorf("farey() = %q, want %q", got, want)
+	}
+	want = Fraction{15, 16}
+	if got := RatApprox(.9375); got != want {
+		t.Errorf("farey() = %q, want %q", got, want)
+	}
+}
